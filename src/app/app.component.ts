6476ac80c677c80215ed7968f362ea56fff74447
Juandi004 <jdgutierrez.2@sudamericano.edu.ec>
2024-01-29 18:04:40 -0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LogoComponent } from './logo/logo.component';
+import { ImagesComponent } from './images/images.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LogoComponent, ImagesComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
